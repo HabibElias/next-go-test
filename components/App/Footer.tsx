99e4afdd2525ddf07f96f0b26e2a8d7db299ba13
Habib Elias <habibelias234@gmail.com>
2025-11-27 03:32:42 -0900

    project setuped and linter added

---
+++ components/App/Footer.tsx
@@ -0,0 +1,5 @@
+function Footer() {
+  return <div>Todo for fun but &copy;2025 copyright reserved</div>
+}
+
+export default Footer
---
+++ components/App/Header.tsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <div className="flex items-center justify-between">
+      <div>Todo App</div>
+      <nav>
+        <ul>
+          <li>All</li>
+          <li>Done</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
